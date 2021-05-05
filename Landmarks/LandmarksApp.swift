588d6e7de53f8b84e43e1f0461bfc269236cc3f9
allanoricil <allanoricilcos@outlook.com>
2021-05-05 18:32:22 +0100

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Allan Oricil on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
